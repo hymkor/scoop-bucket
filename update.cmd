52c200a018a029c37ae988321922813e344e98ab
HAYAMA_Kaoru <iyahaya@nifty.com>
2023-01-06 00:24:37 +0900

    Remove update.cmd from repository

--- update.cmd
+++
@@ -1,6 +0,0 @@
-copy ..\zar\zar.json bucket\.
-copy ..\expect\expect-lua.json bucket\.
-copy ..\gmnlisp\gmnlisp.json bucket\.
-
-type _README.md > README.md
-go run ./bin/update-readme.go >> README.md
